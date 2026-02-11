d5c78d39d3044d7451f6a42348336b50530e1307
zabidenhtf <policyaskrimerov@gmail.com>
2026-02-11 19:41:30 +0200

    Replaced license.txt to LICENSE

--- src/client.cpp
+++ src/client.cpp
@@ -1,3 +1,4 @@
+/* copyright (c) 2026 mykyta polishyk, see LICENSE file for more info */
 #include "stdio.h"
 
 void client_main(){
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,4 @@
-/* copyright (c) 2026 mykyta polishyk, see licence.txt for more info */
+/* copyright (c) 2026 mykyta polishyk, see LICENSE file for more info */
 #include "stdio.h"
 
 void server_main(){
